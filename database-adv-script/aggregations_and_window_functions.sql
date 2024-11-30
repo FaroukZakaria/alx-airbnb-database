8aabb09a6b8cb6324bf696b0bcc50430ee8667e8
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-30 22:09:43 +0200

    Using both of ROW NUMBER and RANK for practice

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,6 +4,7 @@ FROM user
 GROUP BY user.user_id;
 
 SELECT booking.property_id, COUNT(booking.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(booking.booking_id) DESC) AS property_rank
+    RANK() OVER (ORDER BY COUNT(booking.booking_id) DESC) AS property_rank,
+    ROW_NUMBER() OVER (ORDER BY COUNT(booking.booking_id) DESC) AS property_row_number
 FROM booking
 GROUP BY booking.property_id
