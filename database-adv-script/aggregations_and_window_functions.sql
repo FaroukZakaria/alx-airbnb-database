b34c54bd5227a70c5912e34d53bcc92d027930be
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-30 22:08:42 +0200

    Using RANK() instead of ROW_NUMBER + removing comments

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,11 +1,9 @@
--- Write a query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause
 SELECT user.first_name, user.last_name, COUNT(booking.booking_id) AS total_bookings
 FROM user
     LEFT JOIN booking ON user.user_id = booking.user_id
 GROUP BY user.user_id;
 
--- Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received
 SELECT booking.property_id, COUNT(booking.booking_id) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(booking.booking_id) DESC) AS property_rank
+    RANK() OVER (ORDER BY COUNT(booking.booking_id) DESC) AS property_rank
 FROM booking
 GROUP BY booking.property_id
