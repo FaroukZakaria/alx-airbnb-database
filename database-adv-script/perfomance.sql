c3acb1345bf2fd84c6d5973812776c7d4578a6b2
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-30 23:04:10 +0200

    added "WHERE" clause in initial query

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,16 +1,29 @@
-SELECT * FROM booking, user, property, payment
+SELECT *
+FROM booking, user, property, payment
+WHERE
+    booking.user_id = user.user_id
+    AND booking.property_id = property.property_id
+    AND booking.booking_id = payment.booking_id;
 
-EXPLAIN SELECT * FROM booking, user, property, payment
+EXPLAIN
+SELECT *
+FROM booking, user, property, payment
+WHERE
+    booking.user_id = user.user_id
+    AND booking.property_id = property.property_id
+    AND booking.booking_id = payment.booking_id;
 
-SELECT * 
-FROM booking 
-JOIN user ON booking.user_id = user.user_id 
-JOIN property ON booking.property_id = property.property_id 
-JOIN payment ON booking.booking_id = payment.booking_id;
+SELECT *
+FROM
+    booking
+    JOIN user ON booking.user_id = user.user_id
+    JOIN property ON booking.property_id = property.property_id
+    JOIN payment ON booking.booking_id = payment.booking_id;
 
-EXPLAIN 
-SELECT * 
-FROM booking 
-JOIN user ON booking.user_id = user.user_id 
-JOIN property ON booking.property_id = property.property_id 
-JOIN payment ON booking.booking_id = payment.booking_id;
+EXPLAIN
+SELECT *
+FROM
+    booking
+    JOIN user ON booking.user_id = user.user_id
+    JOIN property ON booking.property_id = property.property_id
+    JOIN payment ON booking.booking_id = payment.booking_id;
