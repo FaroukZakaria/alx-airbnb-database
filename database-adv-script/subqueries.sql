e02905528a8dd7a945b8ad4afbc099d6510d82a5
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-30 19:18:49 +0200

    subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,11 @@
+SELECT property_id FROM property p
+WHERE (SELECT r.rating FROM review r WHERE r.property_id = p.property_id) > 4;
+
+SELECT user.first_name, user.last_name
+FROM user
+WHERE user.user_id IN (
+    SELECT booking.user_id
+    FROM booking
+    GROUP BY booking.user_id
+    HAVING COUNT(booking.booking_id) > 3
+);
